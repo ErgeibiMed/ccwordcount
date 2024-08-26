09b02b2212e102e8249d7935ee1e8481c7a8c070
ErgeibiMed <ergeibi.mohamed@gmail.com>
2024-08-26 20:41:22 +0100

    Step 5 completed

--- src/main.rs
+++ src/main.rs
@@ -2,66 +2,79 @@ use clap::{Parser, Subcommand};
 use std::fs;
 
 #[derive(Debug, Parser)] // requires `derive` feature
-#[command(name = "ccwordcount")]
-#[command(bin_name = "ccwc")]
 #[command(about = "a wordcount linux command tool clone", long_about = None)]
-struct WordrCount {
-
-    // #[arg(required=true)]
-    // filename:String,
+struct WordCount {
 
     #[command(subcommand)]
-    command: Commands,
+    command: Option<Commands>,
+
+    #[arg(global=true,required=false)]
+    filename:String,
 }
 
 #[derive(Debug, Subcommand)]
 enum Commands {
     #[command(name="-c")]
-    Count{
-        filename:String,
-    },
+    Count,
     #[command(name="-l")]
-    Lines{filename:String,},
+    Lines,
     #[command(name="-w")]
-    Words{filename:String,},
+    Words,
     #[command(name="-m")]
-    Characters{filename:String,},
+    Characters,
 
 }
 fn main() {
-    let args=WordrCount::parse();
+    let args=WordCount::parse();
     match args.command {
-        Commands::Count{filename} =>{
-            let f= fs::metadata(&filename).map_err(|err|
-                eprintln!("could not get metadata of {file_name} because of {err}", file_name= filename)
+        Some(Commands::Count) =>{
+            let f= fs::metadata(&args.filename).map_err(|err|
+                eprintln!("could not get metadata of {file_name} because of {err}", file_name= args.filename)
             ).unwrap().len();
-            println!(" {f} {file_name}", file_name=filename);
+            println!(" {f} {file_name}", file_name=args.filename);
         },
 
-        Commands::Lines{filename} =>{
-            let f= fs::read_to_string(&filename).map_err(|err|
-                eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
+        Some(Commands::Lines) =>{
+            let f= fs::read_to_string(&args.filename).map_err(|err|
+                eprintln!("could not read to string the {file_name} because of {err}", file_name= args.filename)
             ).unwrap();
             let lines = f.lines().count();
-            println!(" {lines} {file_name}", file_name=filename);
+            println!(" {lines} {file_name}", file_name=args.filename);
         },
-        Commands::Words{filename} =>{
-            let f= fs::read_to_string(&filename).map_err(|err|
-                eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
+        Some(Commands::Words) =>{
+            let f= fs::read_to_string(&args.filename).map_err(|err|
+                eprintln!("could not read to string the {file_name} because of {err}", file_name= args.filename)
             ).unwrap();
             let words = f.split_whitespace().count();
-            println!(" {words} {file_name}", file_name=filename);
+            println!(" {words} {file_name}", file_name=args.filename);
 
         },
-        Commands::Characters{filename} =>{
-            let f= fs::read_to_string(&filename).map_err(|err|
-                eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
+       Some(Commands::Characters) =>{
+            let f= fs::read_to_string(&args.filename).map_err(|err|
+                eprintln!("could not read to string the {file_name} because of {err}", file_name= args.filename)
             ).unwrap();
             let characters = f.chars().count();
-            println!(" {characters} {file_name}", file_name=filename);
+            println!(" {characters} {file_name}", file_name=args.filename);
 
         },
 
 
+        None => {
+                 let f=fs::read_to_string(&args.filename).map_err(|err|
+                eprintln!("could not read to string the {file_name} because of {err}", file_name= args.filename)
+            ).unwrap();
+            let (
+                lines,
+                words,
+                bytes
+            )=
+            (
+                f.lines().count(),
+                f.split_whitespace().count(),
+                f.bytes().count()
+            );
+            println!("   {lines}  {words}  {bytes}  {file_name}",file_name=args.filename);
+
+        },
     }
 }
