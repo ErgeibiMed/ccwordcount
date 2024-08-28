7f8e809b85554966770b01762c7c526f98ff5b0e
ErgeibiMed <ergeibi.mohamed@gmail.com>
2024-08-28 18:52:49 +0100

    refactoring a bit

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is simply an exercise from Build Your Own wc Tool from
 [coding challenges website](https://codingchallenges.fyi/challenges/challenge-wc/)
 
-# Description
+## Description
 
 This is my implementation in rust of the challenge build your own version of the Unix command line tool wc!
 
@@ -19,29 +19,41 @@ engineering tools - allowing us to create very complex text data processing pipe
 You can read more about the Unix Philosophy in the excellent book
 [The Art of Unix Programming](http://www.catb.org/~esr/writings/taoup/html/).
 
-# Usage
- - Cargo run -- -flag filename
- - ccwc -flag filename
+## Quickstart
+
+```console
+ $ cargo build -r
+```
+
+## Usage
+ - ./target/release/ccwc -flag filename
  - flag is one of the following:
 
     - c : for number of bytes in the file
     - l : for number of lines in the file
     - w : for number of words in the file
     - m : for number of characters in the file
+    - h : print the help message
 
  - if no flag is provided then it is equivalent to passing the default option - i.e. no options
    are provided, which is the equivalent to the -l, -w and -c options.
 
  - support being able to read from standard input if no filename is specified
 
-    - cat test.txt | ccwc -l   //   should output : 7145
+## Example
 
-# Example
- - cargo run -- -c test.txt   // should output : 342190 test.txt
- - ccwc -c test.txt           // should output : 342190 test.txt
- - cargo run -- test.txt     //  should output : 7145   58164  342190 test.txt
- - ccwc test.txt            //   should output : 7145   58164  342190 test.txt
-
+```console
+ $ cargo run -- -c test.txt
+ $ 342190 test.txt
+ $ ./target/release/ccwc -c test.txt
+ $ 342190 test.txt
+ $ cargo run -- test.txt
+ $ 7145   58164  342190 test.txt
+ $ ./target/release/ccwc test.txt
+ $ 7145   58164  342190 test.txt
+ $ cat test.txt | ./target/release/ccwc -l
+ $ 7145
+```
 # Dependencies
  [clap](https://github.com/sharkdp/clap-rs)
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::{Parser, Subcommand};
 use std::{fs, io::{self, Read}};
 
 #[derive(Debug, Parser)] // requires `derive` feature
-#[command(about = "a wordcount linux command tool clone", long_about = None)]
+#[command(about = None, long_about = None,disable_help_subcommand=true)]
 struct WordCount {
 
     #[command(subcommand)]
@@ -22,62 +22,69 @@ enum Commands {
     Words,
     #[command(name="-m")]
     Characters,
+    #[command(name="-h")]
+    Help,
+    #[command(name="help")]
+    Longhelp,
+
 
 }
 fn main() {
     let args=WordCount::parse();
     match args.filename{
         None => {
+            let mut data = String::new();
+            let mut stdin= io::stdin();
             match args.command {
+                Some(Commands::Help)| Some(Commands::Longhelp)=> {
+                    println!("");
+                    println!("a wordcount(wc) linux command tool clone");
+                    println!("");
+                    println!("Usage: ccwc <COMMAND> <FILENAME>");
+                    println!("");
+                    println!("Commands: ");
+                    println!("  -c          print the byte counts ");
+                    println!("  -l          print the newline counts ");
+                    println!("  -w          print the word counts ");
+                    println!("  -m          print the character counts ");
+                    println!("  -h, help    print this message ");
+                    println!("");
+
+
+                },
+                Some(Commands::Count) => {
+                    match stdin.read_to_string(&mut data){
+                        Ok(_) => {
+                            println!("  {count} ", count=data.len());
+                        }
+                        Err(error) => println!("error: {error}"),
+                    }
+                },
                 Some(Commands::Lines) => {
-                    let mut data = String::new();
-                    let mut stdin= io::stdin();
                     match stdin.read_to_string(&mut data){
                         Ok(_) => {
                             println!("  {lines} ", lines=data.lines().count());
                         }
                         Err(error) => println!("error: {error}"),
                     }
-
                 },
                 Some(Commands::Words) => {
-                    let mut data = String::new();
-                    let mut stdin= io::stdin();
                     match stdin.read_to_string(&mut data){
                         Ok(_) => {
                             println!("  {words} ", words=data.split_whitespace().count());
                         }
                         Err(error) => println!("error: {error}"),
                     }
-
                 },
                 Some(Commands::Characters) => {
-                    let mut data = String::new();
-                    let mut stdin= io::stdin();
                     match stdin.read_to_string(&mut data){
                         Ok(_) => {
                             println!("  {characters} ", characters=data.chars().count());
                         }
                         Err(error) => println!("error: {error}"),
                     }
-
                 },
-
-                Some(Commands::Count) => {
-                    let mut data = Vec::new();
-                    let mut stdin= io::stdin();
-                    match stdin.read_to_end(&mut data){
-                        Ok(n) => {
-                            println!("  {n} ");
-                        }
-                        Err(error) => println!("error: {error}"),
-                    }
-
-                },
-
                 None => {
-                    let mut data = String::new();
-                    let mut stdin= io::stdin();
                     match stdin.read_to_string(&mut data){
                         Ok(_) => {
                             let (lines,words,bytes)= (data.lines().count(),data.split_whitespace().count(),data.bytes().count());
@@ -90,38 +97,39 @@ fn main() {
             }
         },
         Some(filename)=>{
+            let f= fs::read_to_string(&filename).map_err(|err|
+                eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
+            ).unwrap();
             match args.command {
+                Some(Commands::Help)| Some(Commands::Longhelp)=> {
+                    println!("");
+                    println!("a wordcount(wc) linux command tool clone");
+                    println!("");
+                    println!("Usage: ccwc <COMMAND> <FILENAME>");
+                    println!("");
+                    println!("Commands: ");
+                    println!("  -c          print the byte counts ");
+                    println!("  -l          print the newline counts ");
+                    println!("  -w          print the word counts ");
+                    println!("  -m          print the character counts ");
+                    println!("  -h, help    print this message ");
+                    println!("");
+
+                },
+
                 Some(Commands::Count) =>{
-                    let f= fs::metadata(&filename).map_err(|err|
-                        eprintln!("could not get metadata of {file_name} because of {err}", file_name= filename)
-                    ).unwrap().len();
-                    println!(" {f} {file_name}", file_name=filename);
+                    println!(" {len} {file_name}",len = f.len(), file_name=filename);
                 },
                 Some(Commands::Lines) =>{
-                    let f= fs::read_to_string(&filename).map_err(|err|
-                        eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
-                    ).unwrap();
-                    let lines = f.lines().count();
-                    println!(" {lines} {file_name}", file_name=filename);
+                    println!(" {lines} {file_name}", lines = f.lines().count(),file_name=filename);
                 },
                 Some(Commands::Words) =>{
-                    let f= fs::read_to_string(&filename).map_err(|err|
-                        eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
-                    ).unwrap();
-                    let words = f.split_whitespace().count();
-                    println!(" {words} {file_name}", file_name=filename);
+                    println!(" {words} {file_name}", words = f.split_whitespace().count(),file_name=filename);
                 },
                 Some(Commands::Characters) =>{
-                    let f= fs::read_to_string(&filename).map_err(|err|
-                        eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
-                    ).unwrap();
-                    let characters = f.chars().count();
-                    println!(" {characters} {file_name}", file_name=filename);
+                    println!(" {characters} {file_name}", characters = f.chars().count(),file_name=filename);
                 },
                 None => {
-                    let f=fs::read_to_string(&filename).map_err(|err|
-                        eprintln!("could not read to string the {file_name} because of {err}", file_name= filename)
-                    ).unwrap();
                     let (lines,words,bytes)= (f.lines().count(),f.split_whitespace().count(),f.bytes().count());
                     println!("   {lines}  {words}  {bytes}  {file_name}",file_name=filename);
                 },
